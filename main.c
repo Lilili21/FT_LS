df7a6fd8abc5748342880d3efaf5b7a4ef38e9a1
Kendall Katelynn <kkatelyn@at-f6.21-school.ru>
2019-07-17 16:57:02 +0300

    1

--- main.c
+++ main.c
@@ -7,25 +7,23 @@ void	ft_ls(char **av)
 
 	if (av == NULL)
 		ft_putendl("eq 0");
-	if (!(d = opendir("/Volumes/Storage/goinfre/wned")))
-		perror(ft_strjoin("ft_ls: ", av[1]));
+	if (!(d = opendir(".")))
+		perror(ft_strjoin("ft_ls: ", av[0]));
 	else
-		while ((rd = readdir(d)))
+		while ((rd = readdir(d))) // || rd == NULL && errno )
 		{
-			ft_putstr(rd->d_name);
-			write(1, "\t", 1);
+			//perror(ft_strjoin(rd->d_name, )
+			ft_putendl(rd->d_name);
+		//	write(1, "\t", 1);
 		}
+	//if (closedir(d))
+	//	perror(0);
 }
-
-//void	wile()
 
 int		main(int ac, char **av)
 {
-	//int i;
-
-	while (*av)
-		ft_putendl(*av);
+	printf("%d\n", ac);
 	//if (ac > 1)
-	//	ft_ls(av);
-	
+	//	ft_putendl(av[0]);
+		//ft_ls(av);
 }
