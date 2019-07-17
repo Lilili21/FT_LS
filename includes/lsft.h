a79a859c1635a244664f143e157bcc45ad006509
Kendall Katelynn <kkatelyn@at-f6.21-school.ru>
2019-07-17 20:56:07 +0300

    1

--- includes/lsft.h
+++ includes/lsft.h
@@ -16,4 +16,21 @@
 # include <dirent.h>
 # include <stdio.h>
 
+
+typedef struct	s_q
+{
+	char		*abspath;
+	struct s_q	*next;
+}				t_q;
+
+
+
+typedef struct	s_ls
+{
+	int 		*flags;
+	t_q			*que;
+	char		*abspath;
+	struct s_ls	*next;
+}				t_ls;
+
 #endif
